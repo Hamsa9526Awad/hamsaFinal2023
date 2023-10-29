c54425028a1c03a393e98f7287efe38f7e9b7139
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-10-29 12:14:43 +0200

    JYاكملهن

---
+++ app/src/main/java/awd/example/hamsafinal2023/Data/Signupactivity.java
@@ -0,0 +1,22 @@
+package awd.example.hamsafinal2023.Data;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Signupactivity extends AppCompatActivity {
+
+    private TextInputEditText ETemail;
+    private TextInputEditText ETpass;
+    private TextInputEditText ETrepass;
+    private TextInputEditText ETname;
+    private TextInputEditText ETphone;
+    private Button btnsave;
+    private Button btncancel;
+
+
+
+
+}
