dd3428169a5a68c49aabaed47121eb05fb90abec
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-10-29 12:17:23 +0200

    JYاكملهن

--- app/src/main/java/awd/example/hamsafinal2023/Data/Signupactivity.java
+++ app/src/main/java/awd/example/hamsafinal2023/Data/Signupactivity.java
@@ -1,10 +1,13 @@
 package awd.example.hamsafinal2023.Data;
 
+import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import awd.example.hamsafinal2023.R;
 
 public class Signupactivity extends AppCompatActivity {
 
@@ -15,6 +18,22 @@ public class Signupactivity extends AppCompatActivity {
     private TextInputEditText ETphone;
     private Button btnsave;
     private Button btncancel;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        ETemail= findViewById(R.id.ETemail);
+        ETpass=findViewById(R.id.ETpass);
+        ETrepass= findViewById(R.id.ETrepass);
+        ETname= findViewById(R.id.ETname);
+        ETphone=findViewById(R.id.ETphone);
+        btnsave=findViewById(R.id.btnsave);
+        btncancel=findViewById(R.id.btncancel);
+    }
 
 
 
