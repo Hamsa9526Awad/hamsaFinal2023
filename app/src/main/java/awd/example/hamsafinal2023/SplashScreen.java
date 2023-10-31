6ac564fd22817fba08e78c1e8e3b436ad5aba411
Hamsa Awad <hamsa.awad.123567@gmail.com>
2023-10-31 12:22:37 +0200

    JYاكملهنuhgh

--- app/src/main/java/awd/example/hamsafinal2023/SplashScreen.java
+++ app/src/main/java/awd/example/hamsafinal2023/SplashScreen.java
@@ -2,7 +2,11 @@ package awd.example.hamsafinal2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import awd.example.hamsafinal2023.Data.Signin;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        //start next activity (screen) automatically  after period of time
+        Handler h=new Handler();
+
+
+        Runnable r=new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                //to open new activity from current to next
+                Intent i= new Intent(SplashScreen.this, Signin.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+
+            }
+        };
+        h.postDelayed(r,3000);
     }
 }
