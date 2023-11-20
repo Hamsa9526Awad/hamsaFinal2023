bda9074f77d66b87e05b5d293ff8b81566800c9f
User <hamsa.awad.123567@gmail.com>
2023-11-20 11:37:02 +0200

    tgrrt

---
+++ app/src/main/java/awd/example/hamsafinal2023/Profile.java
@@ -0,0 +1,36 @@
+package awd.example.hamsafinal2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Profile extends AppCompatActivity {
+    private FloatingActionButton addnewdesign;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        addnewdesign=findViewById(R.id.addnewdesign);
+        addnewdesign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //to open new activity from current to next activity
+                Intent i= new Intent(Profile.this,   SignIn.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
+    }
+}
